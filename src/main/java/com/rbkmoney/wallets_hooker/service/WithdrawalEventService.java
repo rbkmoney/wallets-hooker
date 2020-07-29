14b14f9e31d615e7ca3ea4e1c7441e0866d8e9f0
a.romanov <a.romanov@rbkmoney.com>
2020-07-29 13:04:20 +0300

    BJ-969: Add event services

---
+++ src/main/java/com/rbkmoney/wallets_hooker/service/WithdrawalEventService.java
@@ -0,0 +1,36 @@
+package com.rbkmoney.wallets_hooker.service;
+
+import com.rbkmoney.fistful.withdrawal.TimestampedChange;
+import com.rbkmoney.machinegun.eventsink.MachineEvent;
+import com.rbkmoney.sink.common.parser.impl.MachineEventParser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WithdrawalEventService {
+
+    private final List<WithdrawalEventHandler> withdrawalEventHandlers;
+    private final MachineEventParser<TimestampedChange> parser;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void handleEvents(List<MachineEvent> machineEvents) {
+        machineEvents.forEach(this::handleIfAccept);
+    }
+
+    private void handleIfAccept(MachineEvent machineEvent) {
+        TimestampedChange change = parser.parse(machineEvent);
+
+        if (change.isSetChange()) {
+            withdrawalEventHandlers.stream()
+                    .filter(handler -> handler.accept(change))
+                    .forEach(handler -> handler.handle(change, machineEvent));
+        }
+    }
+}
