17bd7a48ea7d1c430b3eaff5a089e2d206e9d3a7
a.romanov <a.romanov@rbkmoney.com>
2020-07-29 13:09:14 +0300

    BJ-969: Add parsers and deserializers

---
+++ src/main/java/com/rbkmoney/wallets_hooker/kafka/serde/WalletChangeDeserializer.java
@@ -0,0 +1,14 @@
+package com.rbkmoney.wallets_hooker.kafka.serde;
+
+import com.rbkmoney.fistful.wallet.TimestampedChange;
+import com.rbkmoney.sink.common.serialization.impl.AbstractThriftBinaryDeserializer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletChangeDeserializer extends AbstractThriftBinaryDeserializer<TimestampedChange> {
+
+    @Override
+    public TimestampedChange deserialize(byte[] bin) {
+        return deserialize(bin, new TimestampedChange());
+    }
+}
