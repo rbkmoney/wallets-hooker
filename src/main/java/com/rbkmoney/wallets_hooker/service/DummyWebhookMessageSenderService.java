ef1a62caf774f6826ad8839323d0ab4309958429
a.romanov <a.romanov@rbkmoney.com>
2020-08-20 11:41:14 +0300

    BJ-969: Add webhook.sender.enabled flag

---
+++ src/main/java/com/rbkmoney/wallets_hooker/service/DummyWebhookMessageSenderService.java
@@ -0,0 +1,26 @@
+package com.rbkmoney.wallets_hooker.service;
+
+import com.rbkmoney.webhook.dispatcher.WebhookMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(
+        name = "webhook.sender.enabled",
+        havingValue = "false")
+public class DummyWebhookMessageSenderService implements WebHookMessageSenderService {
+
+    @PostConstruct
+    public void init() {
+        log.warn("Webhook sending is disabled! Consider setting `webhook.sender.enabled` to `true`");
+    }
+
+    @Override
+    public void send(WebhookMessage webhookMessage) {
+        // do nothing
+    }
+}
